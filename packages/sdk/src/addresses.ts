aa86c22288a6d714f228a7d61897aacd47af5e81
Sollee <solleedev@proton.me>
2023-05-03 19:00:45 +0100

    address template stuff

--- packages/sdk/src/addresses.ts
+++ packages/sdk/src/addresses.ts
@@ -1,6 +1,9 @@
 import { a as addressFoundry } from "../../contracts/out/deploy-31337-latest.json";
+
 const addresses = {
   "31337": { address: addressFoundry, type: "l1" },
+  // "1": {address: "blahblahaddresshere", type: "l1"},
+  // "10": {address: "optimismaddresshere", type: "l2"},
 } as {
   [index: string]: { address: string; type: "l1" | "l2" };
 };
