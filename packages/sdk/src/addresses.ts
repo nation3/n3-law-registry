97208d2de77e53ddb234a579af47d3f1f1b47204
Sollee <solleedev@proton.me>
2023-06-25 10:48:35 +0100

    optimism deployment

--- packages/sdk/src/addresses.ts
+++ packages/sdk/src/addresses.ts
@@ -3,7 +3,7 @@ import { a as addressFoundry } from "../../contracts/out/deploy-31337-latest.jso
 const addresses = {
   "31337": { address: addressFoundry, type: "l1" },
   // "1": {address: "blahblahaddresshere", type: "l1"},
-  // "10": {address: "optimismaddresshere", type: "l2"},
+  "10": { address: "0x002b83f6166f467c430d2154bc30d46f6d5f501c", type: "l2" },
 } as {
   [index: string]: { address: string; type: "l1" | "l2" };
 };
